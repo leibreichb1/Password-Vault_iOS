12fb2b42dfa86a8466570e3ed08d2bc4e7f62083
leibreichb1 <leibreichb1@gmail.com>
2012-10-30 17:02:56 -0400

    got map tracking done

--- Password Vault/PVMapViewController.m
+++ Password Vault/PVMapViewController.m
@@ -72,23 +72,43 @@
     [super viewDidUnload];
 }
 
--(BOOL)timeToPost{
+-(void)checkPostable:(CLLocation *)location{
     int lastPost = [pvm getLastPost];
     NSDate *date = [NSDate date];
     NSDate *prevDate = [[NSDate alloc] initWithTimeIntervalSince1970:lastPost];
     if([date timeIntervalSince1970] - [prevDate timeIntervalSince1970] > (5*60.0)){
-        NSLog(@"TIMER: %f LAST: %f", [date timeIntervalSince1970], [prevDate timeIntervalSince1970]);
         [pvm setLastPost:(int)[date timeIntervalSince1970]];
-        return YES;
+        NSString *user = [pvm getChatUser];
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://devimiiphone1.nku.edu/research_chat_client/password_vault_server/add_location.php"]];
+        [request setHTTPMethod:@"POST"];
+        NSString *postStr = [[NSString alloc] initWithFormat:@"user=%@&lat=%f&lon=%f", user, location.coordinate.latitude, location.coordinate.longitude];
+        NSLog(@"%@", postStr);
+        [request setHTTPBody:[postStr dataUsingEncoding:NSUTF8StringEncoding]];
+        NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+        
+        if(conn){
+            NSLog(@"SENDING...");
+        }
     }
-    NSLog(@"NO TIMER: %f LAST: %f", [date timeIntervalSince1970], [prevDate timeIntervalSince1970]);
-    return NO;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    NSString *res = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", res);
 }
 
 #pragma Location Delegate
 -(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
-    //[locationManager stopUpdatingLocation];
-    [self timeToPost];
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = newLocation.coordinate.latitude;
+    zoomLocation.longitude = newLocation.coordinate.longitude;
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    MKCoordinateRegion adjustedRegion = [_mapView regionThatFits:viewRegion];
+    [_mapView setRegion:adjustedRegion animated:YES];
+    
+    [self checkPostable:newLocation];
 }
 
 @end
